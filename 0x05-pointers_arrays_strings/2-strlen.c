7c112cc76df714007e08a6b6866644bcc572224f
Onyiajerry <jerrynero.jn@gmail.com>
2022-09-19 09:15:38 -0700

    anything

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+*_strlen - returns the lenght of string
+*@s: string to find length
+*Return: length of string
+*/
+int_strlen(char *s)
+{intm = 0;
+while (*(s + m))
+m++;
+return (m);
+}
+
+
