c1a9cc16dcea2362ca6ec23794849b9cd84386da
Onyiajerry <jerrynero.jn@gmail.com>
2022-09-19 15:28:24 -0700

    anything

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -22,7 +22,7 @@ int _strlen(char *s)
  */
 void puts2(char *str)
 {       int b;
-	for (b = 0; str[b] != '\0' && b < _strlen(str); b += 2)
-		(_putchar(str[b]);
+	for (b = 0; str[b] != '\0' && b < _strlen(str) ; b += 2)
+		(_putchar (str[b]);
 	_putchar('\n');
 }
